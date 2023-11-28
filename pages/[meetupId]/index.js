d3cf0f97f8ce30ed8677fccf2f20b31bf69f98d0
Walace Andrade Matos <walace.andr.m@gmail.com>
2023-11-28 13:39:32 -0300

    Created MeetupDetail component and placeholder page.

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/a/a1/1_cristor_redentor_2014.jpg"
+      title="First Meetup"
+      address="Some Street 5, Some City"
+      description="The meetup description"
+    />
+  );
+}
+
+export default MeetupDetails;
