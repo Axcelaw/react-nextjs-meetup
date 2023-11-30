9a59dc0ffc2406fc74ec5a01e24422eb505539fe
Walace Andrade Matos <walace.andr.m@gmail.com>
2023-11-30 16:11:01 -0300

    Use route to send API request for adding new meetup.

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,13 +1,13 @@
 import { MongoClient } from "mongodb";
 
-import { MONGODB_USERNAME, MONGODB_PASSWORD } from "process";
+import * as process from "process";
 
 async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
     const client = await MongoClient.connect(
-      `mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@cluster0.knrr5ck.mongodb.net/meetups?retryWrites=true&w=majority`
+      `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.knrr5ck.mongodb.net/meetups?retryWrites=true&w=majority`
     );
     const db = client.db();
 
